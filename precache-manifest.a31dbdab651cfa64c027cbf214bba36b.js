de9ee740a5b916420ad612f6673d191dc0c780ed
NhanMai <mvtnhan@gmail.cpm>
2020-07-07 20:22:26 +0700

    Updates

---
+++ precache-manifest.a31dbdab651cfa64c027cbf214bba36b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29c1cec45ec9aaa59510b487ae53848",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36e60255f9abd017477e",
+    "url": "./static/css/main.996e5002.chunk.css"
+  },
+  {
+    "revision": "f12db87d9b47d036a76d",
+    "url": "./static/js/2.0f3a472c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.0f3a472c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e60255f9abd017477e",
+    "url": "./static/js/main.b6c40560.chunk.js"
+  },
+  {
+    "revision": "53cd8ffcf59e76a52036",
+    "url": "./static/js/runtime-main.ad9ef463.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "./static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "./static/media/bg.2810fe6b.png"
+  }
+]);
